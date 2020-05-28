5f163338cd8a9c8caeb5d3ad254b052c0211f4bf
dell <dell@192.168.0.103>
2020-05-28 21:18:04 +0530

    Changed Excel Path on Constant Utils folder

--- src/Utility/Constant.java
+++ src/Utility/Constant.java
@@ -4,7 +4,7 @@ public class Constant {
 	
 	public static  final String Url = "http://dev.veri-fuel.com/qa";
 
-    public static final String Path_TestData = "../VF_Test/Resources/";
+    public static final String Path_TestData = "C:\\Users\\dell\\workspace\\VF_Hybrid\\Resources\\";
 
      public static final String File_TestData = "VF_Buyer_Quote_Creation.xlsx";
 
